8b6a75bfb003c3c1fc9e687ec00e01bf443a9a26
Spy132 <ruslan.bitrek@gmail.com>
2025-09-19 11:55:19 +0300

    либы закончены

---
+++ lab02/src/tuples.py
@@ -0,0 +1,34 @@
+def format_record(rec: tuple[str, str, float]) -> str | type[ValueError] | type[TypeError]:
+    """
+    Форматирует запись о студенте в строку вида:
+    "Фамилия И.О., гр. GROUP, GPA X.XX".
+
+    Аргументы:
+        rec (tuple[str, str, float]):
+            - rec[0]: строка с ФИО (2 или 3 слова).
+            - rec[1]: название группы (строка, не пустая).
+            - rec[2]: средний балл (float или int).
+
+    Возвращает:
+        str: Отформатированная строка с фамилией, инициалами, группой и GPA.
+        ValueError: Если не указана группа или неверно задано ФИО.
+        TypeError: Если средний балл не является числом (float или int).
+
+    Примеры:
+        >>> format_record(("иванов иван иванович", "пи-101", 4.25))
+        'Иванов И.И., гр. ПИ-101, GPA 4.25'
+
+        >>> format_record(("сидоров петр", "пи-102", 3))
+        'Сидоров П.П., гр. ПИ-102, GPA 3.00'
+    """
+    if len(rec[1]) == 0: return ValueError('Гиде группа??')
+    if not isinstance(rec[2], float) and not isinstance(rec[2], int): return TypeError('что ты сюда вписал?')
+    try:
+        n1,n2,n3 = rec[0].strip().split()
+        return f"{n1[0].upper() + n1[1:]} {n2[0]}.{n3[0] if n3 else n2[0]}., гр. {rec[1].upper()}, GPA {rec[2]:.2f}"
+    except:
+        pass
+    try:
+        n1,n2 = rec[0].strip().split()
+        return f"{n1[0].upper() + n1[1:]} {n2[0]}.{n2[0]}., гр. {rec[1].upper()}, GPA {rec[2]:.2f}"
+    except Exception as e: print(e); return ValueError('Неверное ФИО')
---
+++ lab02/src/tuples_test.py
@@ -0,0 +1,6 @@
+import tuples
+# Тесты tuples
+print('Testing tuples')
+print("(\"Иванов Иван Иванович\", \"BIVT-25\", 4.6) Out:", tuples.format_record(("Иванов Иван Иванович", "BIVT-25", 4.6)))
+print("(\"Петров Пётр\", \"IKBO-12\", 5.0) Out:", tuples.format_record(("Петров Пётр", "IKBO-12", 5.0)))
+print("(\" сидорова анна сергеевна \", \"ABB-01\", 3.999) Out:", tuples.format_record((" сидорова анна сергеевна ", "ABB-01", 3.999)))
