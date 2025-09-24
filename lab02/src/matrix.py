54cdab9c77536b3674b086df0396212679448001
Spy132 <ruslan.bitrek@gmail.com>
2025-09-24 14:29:01 +0300

    аааа

--- lab02/src/matrix.py
+++ lab02/src/matrix.py
@@ -5,6 +5,7 @@ def transpose(mat: list[list[float | int]]) -> list[list[float | int]] | type[Va
     for item in mat:
         if len(item) != row_length: return ValueError
     return [[row[i] for row in mat] for i in range(row_length)]
+
 def col_sum(mat: list[list[float | int]]) -> list[float] | type[ValueError]:
     if not mat: return []
     row_length = len(mat[0])
